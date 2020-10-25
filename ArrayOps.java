6a8311d2f50e63819ee7c8e5987641ba3de5800d
Rickey Dong <rickeyd2004@gmail.com>
2020-10-25 10:47:01 -0400

    finished the skeleton of ArrayOps.java. implemented sum(int[] arr) method, tested it, and it works

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,9 +2,14 @@ public class ArrayOps
 {
   public static int sum(int[] arr)
   {
-
+    int total = 0;
+    for (int number = 0; number < arr.length; number++)
+    {
+      total = total + arr[number];
+    }
+    return total;
   }
-  public static int largest(int[] arr)
+  /*public static int largest(int[] arr)
   {
 
   }
@@ -20,4 +25,20 @@ public class ArrayOps
   {
 
   }
+  public static int[] sumCols(int[][] matrix)
+  {
+
+  }
+  public static boolean isRowMagic(int[][] matrix)
+  {
+
+  }
+  public static boolean isColMagic(int[][] matrix)
+  {
+
+  }
+  public static boolean isLocationMagic(int[][] matrix, int row, int col)
+  {
+
+  }*/
 }
--- Tester.java
+++ Tester.java
@@ -2,6 +2,11 @@ public class Tester
 {
   public static void main(String[] args)
   {
-    
+    int[] a = {3, 5, 7, 6, 4};
+    System.out.println(ArrayOps.sum(a) + " should be 25");
+    int[] b = {2, 7, 0, 4};
+    System.out.println(ArrayOps.sum(b) + " should be 13");
+    int[] c = {};
+    System.out.println(ArrayOps.sum(c) + " should be 0");
   }
 }
