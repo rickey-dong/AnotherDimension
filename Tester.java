45612c2d1eaa5519c3ef5415b09480e94a61d3ac
Rickey Dong <rickeyd2004@gmail.com>
2020-10-25 11:26:44 -0400

    imported java.util.Arrays

--- Tester.java
+++ Tester.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Tester
 {
   public static void main(String[] args)
@@ -15,5 +17,8 @@ public class Tester
     System.out.println(ArrayOps.largest(largestb) + " should be 9");
     int[] largestc = {3, 4, 6, 8, 13, 20};
     System.out.println(ArrayOps.largest(largestc) + " should be 20");
+    System.out.println("------------------------------------");
+    int[][] konstanA = { {1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9} };
+    System.out.println(Arrays.toString(ArrayOps.sumRows(konstanA)) + " should be [12, 8, -14]");
   }
 }
